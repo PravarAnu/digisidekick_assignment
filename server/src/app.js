1c43354804b980edd44d8bf8b818a2768865185b
Pravar Anu <pravaranu6@gmail.com>
2023-07-16 00:03:21 +0530

    Installed some library and used it as middleware for express app

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cookieParser());
 
 
 app.get("/", (req, res)=>{
