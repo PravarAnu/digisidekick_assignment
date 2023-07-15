56a8489f13e713172ea7ceb6db5faea902260cda
Pravar Anu <pravaranu6@gmail.com>
2023-07-15 23:59:26 +0530

    Added JWT Secret and Expiry

--- server/src/config/index.config.js
+++ server/src/config/index.config.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT,
-    MONGODB_URL: process.env.MONGODB_URL
+    MONGODB_URL: process.env.MONGODB_URL,
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 
 export default config;
