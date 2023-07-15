0f09b09b7884b8aa9caf3b51c15ac529dd9d4550
Pravar Anu <pravaranu6@gmail.com>
2023-07-15 23:26:48 +0530

    Handled unwanted route calling

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
+app.get("/", (req, res)=>{
+    res.send("Hi! You are at wrong route. Please, Check it.")
+})
 
+app.all("*", (req, res)=>{
+    return res.status(404).json({
+        success: false,
+        message: "You are at wrong route"
+    })
+})
 
 export default app;
