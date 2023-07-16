9e77d1a9fe92bbf7488ab3e5f1545ce5465dbf61
Pravar Anu <pravaranu6@gmail.com>
2023-07-16 19:23:43 +0530

    Defined the api call url starts with

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import routes from "./routes/index.route.js"
+
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(cookieParser());
 
+
+app.use("/digisidekick/v1", routes);
 
 app.get("/", (req, res)=>{
     res.send("Hi! You are at wrong route. Please, Check it.")
